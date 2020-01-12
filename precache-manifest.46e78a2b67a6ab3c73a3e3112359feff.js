6cbbafaa1f4623652cf8416de65a7465d270ef2f
Alex <alexmoller02@hotmail.com>
2020-01-12 22:16:27 +0100

    Updates

---
+++ precache-manifest.46e78a2b67a6ab3c73a3e3112359feff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ea4a73e10a8053b6c155ee5e66c989",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "232e294eed3c1af0c6d0",
+    "url": "/portfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "e957e1f07c289e3e39aa",
+    "url": "/portfolio/static/css/main.9f35549c.chunk.css"
+  },
+  {
+    "revision": "232e294eed3c1af0c6d0",
+    "url": "/portfolio/static/js/2.a5fc3e26.chunk.js"
+  },
+  {
+    "revision": "369c35024efebe5c40542bf0ba67a549",
+    "url": "/portfolio/static/js/2.a5fc3e26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e957e1f07c289e3e39aa",
+    "url": "/portfolio/static/js/main.93dfacc4.chunk.js"
+  },
+  {
+    "revision": "e001840f84fb37184e3e",
+    "url": "/portfolio/static/js/runtime-main.adc79c8e.js"
+  },
+  {
+    "revision": "e18977fd23c4850b464c07ab3d5e9f32",
+    "url": "/portfolio/static/media/mountains.e18977fd.png"
+  }
+]);
