46d9704c548e6916802e0efd1eabb3362d6f6335
Alex <alexmoller02@hotmail.com>
2020-01-12 22:08:36 +0100

    first commit

---
+++ src/components/aboutme.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return(
+      <div className="aboutme">
+        <div className="abouttitle">
+          <h1>About me</h1>
+        </div>
+        <div class="container">
+          <div class="parralax">
+
+            </div>
+      </div>
+      <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+      <p>My name is Alex Møller, i am 17 years old and i come from Denmark. I live in a small town called Thorsø, and i currently study at Aarhus tech in Viby.</p>
+      <p> I love parkour and have been training for 7 years now, i even went to BGI which is the boarding school with the worlds biggest private parkour area.</p>
+      <p> I have been a parkour instructor for around 4 years, i have been working in 2 different clubs, TIF(Thorsø idrætsforening) and RGF(Randers gymnastiske forening).</p>
+      <p> Other than that i really like to program too, i've been programming since i was 10 years old, back then i started out with batch. Now i have experince in Batch, HTML/CSS, React, Node.js, Python and Javascript</p>
+      <p> Currently i have no job, but i have had jobs in the past, as parkour instructor, and i helped out in some renting company for parties. I do smaller jobs too, this year i get to count points in hiphopinternational DK, i made their poster as well. </p>
+
+    </div>
+
+    )
+  }
+}
+
+export default About;
